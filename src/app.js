fc7f426f8f5431c1b40e4e89157960150de9c343
Octav Zaharia <ozaharia@wehkamp.nl>
2017-07-05 14:28:33 +0300

    Add model

--- src/app.js
+++ src/app.js
@@ -44,11 +44,26 @@ function intent(sources) {
   return xs.merge(inputLogin$, inputPassword$);
 }
 
+function model(action$) {
+  var state = {};
+
+  return (action$.map(action => {
+      if(action.type === 'inputLogin') {
+        state =  {...state, login: action.payload}
+      } else if(action.type === 'inputPassword') {
+        state = {...state, password: action.payload}
+      }
+      return state;
+    }
+  ));
+}
+
 export function App (sources) {
-  const action$ = intent(sources)/*.debug("actions")*/;
-  action$.addListener({
-    next: (value) => console.log("=>", value)
-  })
+  const action$ = intent(sources).debug("action");
+  const state$ = model(action$).startWith({}).debug("state");
+  state$.addListener({
+    next: (v) => console.log(v)
+  });
   const vtree$ = xs.of(
     view()
   );
